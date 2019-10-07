5b14217c269ba79349eec048a9f7cb36ce135d80
Павел <49659752+PavelSher09@users.noreply.github.com>
2019-10-07 22:28:39 +0300

    Add files via upload

--- bank.rb
+++ bank.rb
@@ -1,5 +1,19 @@
 
 
+require_relative 'deck'
+require_relative 'card'
+require_relative 'gamer'
+require_relative 'bank'
+require_relative 'game'
+
 class Bank
+
+
+attr_reader :money
+
+  def initialize
+    @money = 100
+  end
+
 
 end
